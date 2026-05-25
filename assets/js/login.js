8b604baa07fcfc4c5927812570f2062a43ee3283
Filipe <filipejosecamargo2014@gmail.com>
2026-05-25 08:35:17 -0300

    Adicionado animação no input do login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,6 @@
-btnVoltar = document.getElementById("btn-voltar");
-
+const btnVoltar = document.getElementById("btn-voltar");
+const inputs = document.querySelectorAll("input");
+const labels = document.querySelectorAll("label");
 
 btnVoltar.addEventListener("click", () => {
     btnVoltar.disabled = true;
@@ -11,3 +12,38 @@ btnVoltar.addEventListener("click", () => {
         btnVoltar.disabled = false
     }
 })
+
+for (i = 0; i < inputs.length; i++) {
+    let input = inputs[i];
+    let label = labels[i];
+
+    input.addEventListener("", () => {
+        label.className = "label-focus";
+    })
+
+    input.addEventListener("focusin", () => {
+        label.classList.add("label-focus");
+    })
+
+    input.addEventListener("mouseenter", () => {
+        label.classList.add("label-focus");
+    })
+
+    input.addEventListener("focusout", () => {
+        if (input.value === "") {
+            label.classList.remove("label-focus");
+        }
+    })
+
+    input.addEventListener("mouseout", () => {
+        if (input.value === "") {
+            label.classList.remove("label-focus");
+        }
+    })
+
+    input.addEventListener("change", () => {
+        if (input.value != "") {
+            label.className = "label-focus";
+        }
+    })
+}
